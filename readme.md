9a7d37b3430302669cc7c742a10cf03a79ef528f
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-07-16 09:52:06 -0400

    hello weekend project

---
+++ readme.md
@@ -0,0 +1,5 @@
+
+# JWST imagery analysis
+
+
+
---
+++ threedeeify.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import traceback
+import time
+import shutil
+
+
+def dl_imagery(target_dir='out'):
+  pass
+
+
+def main(args=sys.args):
+  dl_imagery()
+
+
+if __name__ == '__main__':
+  main()
+
